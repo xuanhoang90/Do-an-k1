b5b3968e307211d6cb464b8073fef788d811cd26
Xuan Hoang <xuanhoang@jv-it.com.vn>
2024-12-17 22:47:14 +0700

    refactor

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Forms</title>
+  <title>Admin</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <style>
     #loader {
